455eef87631d8077fe636f6106efbd8b842faaed
kostyapoymenov <>
2025-10-28 22:04:45 +0300

    fix

--- server.cjs
+++ server.cjs
@@ -6,6 +6,9 @@ const { port = 3000 } = process.env;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`);
 });
